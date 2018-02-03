315ba2fd8899fb81f5146c0f8a27254a395ae35b
“Louis <2268980C@student.gla.ac.uk>
2018-02-03 15:22:21 +0000

    update Chapter 6

---
+++ rango/views.py
@@ -0,0 +1,34 @@
+from django.shortcuts import render
+from rango.models import Category
+from django.http import HttpResponse
+from rango.models import Page
+
+
+def index(request):
+
+    category_list = Category.objects.order_by('-likes')[:5]
+    context_dict = {'categories': category_list}
+    # Return a rendered response to send to the client.
+    # We make use of the shortcut function to make our lives easier.
+    # Note that the first parameter is the template we wish to use.
+    return render(request, 'rango/index.html', context_dict)
+
+
+def about(request):
+    return render(request, 'rango/about.html',{})
+
+
+def show_category(request, category_name_slug):
+
+    context_dict = {}
+
+    try:
+        category = Category.objects.get(slug=category_name_slug)
+        pages = Page.objects.filter(category=category)
+        context_dict['pages'] = pages
+        context_dict['category'] = category
+    except Category.DoesNotExist:
+        context_dict['category'] = None
+        context_dict['pages'] = None
+
+    return render(request, 'rango/category.html', context_dict)
